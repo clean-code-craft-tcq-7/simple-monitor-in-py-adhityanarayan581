bc9a0bb60e2ef6c241e8b99c6b322a6f6999979b
Adhitya Narayan <64026841+adhityanarayan581@users.noreply.github.com>
2025-07-29 10:00:03 +0000

    Modified monitor and monitor test

--- monitor.py
+++ monitor.py
@@ -1,37 +1,26 @@
-
 from time import sleep
 import sys
 
+def blink_warning():
+  for _ in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
     print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    blink_warning()
     return False
   elif pulseRate < 60 or pulseRate > 100:
     print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    blink_warning()
     return False
   elif spo2 < 90:
     print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    blink_warning()
     return False
   return True
--- monitor.test.py
+++ monitor.test.py
@@ -3,10 +3,27 @@ from monitor import vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    def test_temperature_too_high(self):
+        self.assertFalse(vitals_ok(103, 80, 95))  # High temp
+
+    def test_temperature_too_low(self):
+        self.assertFalse(vitals_ok(94, 80, 95))   # Low temp
+
+    def test_pulse_too_low(self):
+        self.assertFalse(vitals_ok(98, 55, 95))   # Low pulse
+
+    def test_pulse_too_high(self):
+        self.assertFalse(vitals_ok(98, 105, 95))  # High pulse
+
+    def test_spo2_too_low(self):
+        self.assertFalse(vitals_ok(98, 80, 89))   # Low SpO2
+
+    def test_all_vitals_normal(self):
+        self.assertTrue(vitals_ok(98.6, 75, 96))  # All normal
+
+    def test_first_vital_out_short_circuit(self):
+        self.assertFalse(vitals_ok(103, 120, 85))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
